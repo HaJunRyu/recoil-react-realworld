441673fc8825fc6625bd504bae9fcc2f2275fb87
HaJunRyu <hjr4557@naver.com>
2022-07-03 22:24:07 +0900

    Fix: element의 attribute값을 지정된 타입으로 전달하게끔 수정

--- src/pages/article/Article.tsx
+++ src/pages/article/Article.tsx
@@ -73,11 +73,7 @@ function ArticlePage() {
           <div className="col-xs-12 col-md-8 offset-md-2">
             <form className="card comment-form">
               <div className="card-block">
-                <textarea
-                  className="form-control"
-                  placeholder="Write a comment..."
-                  rows="3"
-                ></textarea>
+                <textarea className="form-control" placeholder="Write a comment..." rows={3} />
               </div>
               <div className="card-footer">
                 <img src="http://i.imgur.com/Qr71crq.jpg" className="comment-author-img" />
